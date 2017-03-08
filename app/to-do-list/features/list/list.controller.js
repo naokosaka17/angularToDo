0c2b76bf91b4d662eea79f2c968194ea29f315cd
NAO <Nao@NaoKosaka.local>
2017-03-07 19:25:46 -0500

    create done

--- app/to-do-list/features/list/list.controller.js
+++ app/to-do-list/features/list/list.controller.js
@@ -2,7 +2,8 @@ angular
     .module('toDo.list') // No array, as we are "extending" the module
     .controller('ListCtrl', ListCtrl); // Define our controller (Notice the naming convention - uppercase first letter, Ctrl suffix)
 
-function ListCtrl() {
-     // vm stands for View Model - anything on "the vm" is exposed to the view
-
+function ListCtrl($http) {
+     $http.get('/api/todos').then(function(data){
+       console.log(data);
+     });
 }
--- server.js
+++ server.js
@@ -1,8 +1,71 @@
+// set up ======================================================================
+// dependencies
 var express = require('express');
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+//express setup
 var app = express();
 var port = 3000;
+//import Todo models
+var Todo = require("./app/models/ToDo.js");
 
+// configuration ===============================================================
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+//starting point
 app.use(express.static(__dirname + "/app"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/angularToDo");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+});
+
+//body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded ({ extended:true }));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// routes ===============================================================
+// get all todos
+    app.get('/api/todos', function(req, res) {
+
+        // use mongoose to get all todos in the database
+        Todo.find({},function(err, todos) {
+
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err){
+              res.send(err);
+            }
+            res.json(todos); // return all todos in JSON format
+        });
+    });
+
+// create todo and send back all todos after creation
+    app.post('/api/todos', function(req, res) {
+      console.log(req.body);
+        // create a todo, information comes from AJAX request from Angular
+        var entry = new Todo(req.body);
+        entry.save(function(err, doc) {
+          if (err) {
+            res.json(err);
+          }
+          else {
+            console.log(doc);
+            res.json(doc);
+          }
+        });
+    });
 
 app.listen(port, function() {
     console.log("listening on port:" + port);
