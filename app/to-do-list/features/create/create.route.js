17996b5d80898e85f4f46ab66189b754bf115a7d
NAO <Nao@NaoKosaka.local>
2017-03-09 00:52:57 -0500

    list shows on terminal only

--- app/to-do-list/features/create/create.route.js
+++ app/to-do-list/features/create/create.route.js
@@ -3,11 +3,6 @@ angular
     .config(createConfig);
 
 function createConfig($stateProvider) {
-    // $stateProvider is an Angular service exposed by the ui-router library
-    // this service allows us to define the "states" for our application
-    //
-    // REMEMBER: AngularJS is a single-page application framework. This means we
-    // don't have the concept of "pages" but instead we use the concepts of "states"
     $stateProvider.state({
         name: 'create', // name of the route
         url: '/create', // url endpoint for the route
--- app/to-do-list/features/list/list.controller.js
+++ app/to-do-list/features/list/list.controller.js
@@ -3,6 +3,12 @@ angular
     .controller('ListCtrl', ListCtrl); // Define our controller (Notice the naming convention - uppercase first letter, Ctrl suffix)
 
 function ListCtrl($http) {
+  var vm = this;
+  vm.data = {
+    text: '',
+    date: ''
+  };
+
      $http.get('/api/todos').then(function(data){
        console.log(data);
      });
