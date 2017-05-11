120e3ed0c4852ad3469142570a02d3968e247817
NAO <naokosaka17@gmail.com>
2017-05-11 19:20:16 -0400

    finish list next completed

--- app/to-do-list/features/list/list.controller.js
+++ app/to-do-list/features/list/list.controller.js
@@ -6,8 +6,26 @@ function ListCtrl($http) {
   var vm = this;
 
      $http.get('/api/todos').then(function(cb){
-       console.table(cb.data);
        vm.list = cb.data;
+      //get current time
+       var time = new Date().getTime();
+       //  console.log(new Date(time)); return: Thu May 11 2017 18:58:30 GMT-0400 (EDT)
+
+       // get index 0 to 15 -> Thu May 11 2017
+       vm.dateTod = new Date(time).toString().slice(0,15);
+      //returns number of milliseconds
+       vm.date = Date.now();
+
+        // loop through each object in mongo
+         for (var i=0;i<cb.data.length; i++) {
+            //change mongo time(2017-5-11T10:32:32) to seconds
+             cb.data[i].dueDate = (new Date(cb.data[i].dueDate)).getTime();
+            //change milliseconds to human readable date for Due today
+             cb.data[i].isTod = (new Date(cb.data[i].dueDate).toString().slice(0,15))
+             console.log(cb.data[i].isTod)
+
+         }
+
      });
 
      vm.check = function(id){
